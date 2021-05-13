ad88935e64f60ce7c058bbd4742fb44262260440
Роман Роман <romazen@i.ua>
2021-05-13 12:45:43 +0300

    Made a check for the uniqueness of tags

--- Blog.BLL/Services/BlogService.cs
+++ Blog.BLL/Services/BlogService.cs
@@ -21,36 +21,41 @@ namespace Blog.BLL.Services
         }
         public async Task<OperationDetails> Create(BlogDto blogDto)
         {
-            if(blogDto != null)
+            if (blogDto != null)
             {
-                Regex regex = new Regex(@"(?<=#)\w+");
-                MatchCollection matches = regex.Matches(blogDto.Text);
                 List<Tag> tags = new List<Tag>();
-                foreach (Match tag in matches)
+                Regex regex = new Regex(@"(?<=#)\w+");
+                IEnumerable<string> uniqueTags = Unique(regex.Matches(blogDto.Text));
+
+                if(uniqueTags != null)
                 {
-                    string tagValue = tag.Value;
-                    Tag result = _uow.TagRepository.GetFirstOrDefault(x => x.Body == tagValue);
-                    if (result == null)
+                    foreach (string tag in uniqueTags)
                     {
-                        tags.Add(new Tag { Body = tagValue });
-                    } 
-                    else
-                    {
-                        tags.Add(result);
+                        Tag result = _uow.TagRepository.GetFirstOrDefault(x => x.Body == tag);
+
+                        if (result == null)
+                        {
+                            tags.Add(new Tag { Body = tag });
+                        }
+                        else
+                        {
+                            tags.Add(result);
+                        }
                     }
                 }
 
                 Post post = new Post { Title = blogDto.Title, Text = blogDto.Text, CreateAt = blogDto.CreateAt, IsDeleted = blogDto.IsDeleted, UserProfileId = blogDto.UserProfileId, Tag = tags };
 
                 Post createdPost = _uow.PostRepository.Create(post);
-                
+
                 await _uow.SaveAsync();
                 return new OperationDetails(true, "Blog has been successfully created", "");
-            } else
+            }
+            else
             {
                 return new OperationDetails(false, "Things went wrong... maybe blog is empty", "");
             }
-            
+
         }
 
         public async Task<OperationDetails> AddComment(CommentDto commentDto)
@@ -78,9 +83,9 @@ namespace Blog.BLL.Services
                 cfg.CreateMap<Comment, CommentDto>();
             });
 
-           Mapper mapper = new Mapper(config);
-           List<BlogDto> userBlogs = mapper.Map<List<BlogDto>>(_uow.PostRepository.GetWithInclude(x => x.UserProfileId == Id, p => p.UserProfile, c => c.Comment));
-           return userBlogs;
+            Mapper mapper = new Mapper(config);
+            List<BlogDto> userBlogs = mapper.Map<List<BlogDto>>(_uow.PostRepository.GetWithInclude(x => x.UserProfileId == Id, p => p.UserProfile, c => c.Comment));
+            return userBlogs;
         }
 
         public List<BlogDto> GetAllBlogs()
@@ -100,5 +105,29 @@ namespace Blog.BLL.Services
         {
             _uow.Dispose();
         }
+
+        private IEnumerable<string> Unique(MatchCollection matches)
+        {
+            List<string> tags = null;
+            IEnumerable<string> uniqueTags = null;
+
+            if (matches != null)
+            {
+                tags = new List<string>();
+
+                foreach (Match tag in matches)
+                {
+                    tags.Add(tag.Value.ToLower());
+                }
+
+                uniqueTags = tags.Distinct<string>();
+
+                return uniqueTags;
+
+            }
+
+            return uniqueTags;
+        }
     }
 }
+
